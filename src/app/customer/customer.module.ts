dcefadfc9598b4c70f31b676fdad973c99406a73
Xtian <xtiancuenta1@gmail.com>
2023-02-06 12:35:51 -0300

    Customer components and service
    Customer components and one service were created

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EditComponent } from './edit/edit.component';
+import { DeleteComponent } from './delete/delete.component';
+import { ListInfoComponent } from './list-info/list-info.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EditComponent,
+    DeleteComponent,
+    ListInfoComponent
+  ],
   imports: [
     CommonModule
   ]
