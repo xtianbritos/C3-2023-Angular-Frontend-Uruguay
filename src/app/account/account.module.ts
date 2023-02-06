5d1060c5e9161685a6210226d9d8044e1e9c2b83
Xtian <xtiancuenta1@gmail.com>
2023-02-06 11:40:30 -0300

    Crud components Account
    Crud components created to Account module

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { DeleteComponent } from './delete/delete.component';
+import { ListComponent } from './list/list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateComponent,
+    EditComponent,
+    DeleteComponent,
+    ListComponent
+  ],
   imports: [
     CommonModule
   ]
