ccc29f5d84b4da4f6c734e25cf8a512026a2473e
Xtian <xtiancuenta1@gmail.com>
2023-02-09 08:39:54 -0300

    Routes created
    Routes was created for default, sign up and sign in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SingupComponent } from './login/singup/singup.component';
+import { SinginComponent } from './login/singin/singin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'signin',
+    pathMatch: 'full'
+  },
+  {
+    path: 'signin',
+    component: SinginComponent
+  },
+  {
+    path: 'signup',
+    component: SingupComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
